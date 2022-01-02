0638f96ab75acdf145b6424fbcca0b1cfed322de
Chester Luna <5784451-.chluna@users.noreply.gitlab.com>
2022-01-02 16:31:00 +0800

    scripts: comment out ytfzf in rofi

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -5,14 +5,14 @@ SCRIPT_DIR="/home/spike/.local/bin"
 echo -en "\x00prompt\x1fscript  >>\n"
 
 if [[ -z "$@" ]]; then
-    echo -e "youtube"
+    #echo -e "youtube"
     #echo -e "password\nyoutube"
 else
     case "$@" in
         #password)
         #    coproc (zsh -c "$SCRIPT_DIR/rofi-keepassxc-cli" > /dev/null);;
-        youtube)
-            coproc (YTFZF_EXTMENU=' rofi -i -dmenu -fuzzy -width 1500 -l 20 -p youtube:' ytfzf -D > /dev/null);;
+        #youtube)
+        #    coproc (YTFZF_EXTMENU=' rofi -i -dmenu -fuzzy -width 1500 -l 20 -p youtube:' ytfzf -D > /dev/null);;
         *)
             exit 1
     esac
