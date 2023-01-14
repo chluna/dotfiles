dd8a5f0db4ea089a7412f76e2f5b44a93a5c23bd
Chester Luna <5784451-.chluna@users.noreply.gitlab.com>
2023-01-14 22:02:51 +0800

    genmon: update internal name in scripts

--- root/etc/NetworkManager/dispatcher.d/99-update-genmon-network.sh
+++ root/etc/NetworkManager/dispatcher.d/99-update-genmon-network.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  
 su spike -c "DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus \
-  /usr/bin/xfce4-panel --plugin-event=genmon-17:refresh:bool:true --display=:0"
+  /usr/bin/xfce4-panel --plugin-event=genmon-10:refresh:bool:true --display=:0"
