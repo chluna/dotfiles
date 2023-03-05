aeafbf4939bd0c056a7bedf4f96e3e1f4f585715
Chester Luna <5784451-.chluna@users.noreply.gitlab.com>
2023-03-05 13:44:03 +0800

    lightdm: replace lightdm-mini-greeter with web-greeter

--- run_onchange_after_system-config.sh
+++ run_onchange_after_system-config.sh
@@ -26,11 +26,11 @@ sudo ln -sf $ZDOTDIR /root/.config
 sudo ln -sf /home/spike/.config/helix/ /root/.config
 
 # Copy configuration files to corresponding directories in /etc
-sudo cp "$root"/etc/btrbk/btrbk.conf /etc/
+sudo cp "$root"/etc/btrbk/btrbk.conf /etc/btrbk
+sudo cp "$root"/etc/lightdm/web-greeter.yml /etc/lightdm
 sudo cp "$root"/etc/NetworkManager/dispatcher.d/99-update-genmon-network.sh /etc/NetworkManager/dispatcher.d/
 sudo cp "$root"/etc/pacman.conf /etc/
 sudo cp "$root"/etc/throttled.conf /etc/
-sudo cp "$root"/etc/lightdm/lightdm-mini-greeter.conf /etc/
 
 # Configure NetworkManager-dispatcher genmon script
 sudo chown root:root /etc/NetworkManager/dispatcher.d/99-update-genmon-network.sh
