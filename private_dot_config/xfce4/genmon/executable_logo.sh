20e05c49aba381c8c16d9792467c591c71ff62e9
Chester Luna <5784451-.chluna@users.noreply.gitlab.com>
2023-04-30 13:40:31 +0800

    genmon: update scripts

--- private_dot_config/xfce4/genmon/executable_logo.sh
+++ private_dot_config/xfce4/genmon/executable_logo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 txt="<txt>"
-txt+="  "
+txt+=""
 txt+="</txt>"
 
 tool="<tool>"
--- private_dot_config/xfce4/genmon/executable_network.sh
+++ private_dot_config/xfce4/genmon/executable_network.sh
@@ -9,13 +9,13 @@ ssid="$(nmcli -p device show wlp3s0 | grep GENERAL.CONNECTION | tr -s ' ' | cut 
 eth="$(nmcli -p device show enp0s31f6 | grep IP4.ADDRESS | tr -s ' ' | cut -d ' ' -f 2 | cut -d '/' -f 1)"
 
 if [[ ! -z "$wlan" && ! -z "$eth" ]]; then
-    txt+="直 wifi   wire"
-    tool+="ssid: $ssid\nwlan: $wlan\nlan: $eth"
+    txt+="直 $ssid   eth"
+    tool+="wlan: $wlan\nlan: $eth"
 elif [[ ! -z "$wlan" && -z "$eth" ]]; then
-    txt+="直 wifi"
-    tool+="ssid: $ssid\nwlan: $wlan"
+    txt+="直 $ssid"
+    tool+="wlan: $wlan"
 elif [[ -z "$wlan" && ! -z "$eth" ]]; then
-    txt+=" wire"
+    txt+=" eth"
     tool+="lan: $eth" 
 else
     txt+="泌"
@@ -26,7 +26,7 @@ public="$(curl ipinfo.io | jq -r '.ip')"
 if [[ ! -z "$public" ]]; then
     tool+="\npublic: $public"
 else
-    txt+=" (offline)"
+    txt+=" offline"
 fi
 
 txtclick+="bash -c '~/.config/xfce4/genmon/network_reset.sh "$ssid"'"
